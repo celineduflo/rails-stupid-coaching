a326e8873838d82c8a7d91063149789a6b49e39f
Celine Duflo <cduflo@hotmail.com>
2020-02-12 15:11:54 +0100

    new computer

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'ask', to: 'questions#ask', as: :askpage
+  get 'answer', to: 'questions#answer'
+  root 'questions#ask'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
